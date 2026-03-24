2b150554d37fa8c6efb6705522c4040814771a87
CauichDev <cauichdev@gmail.com>
2026-03-23 23:59:39 -0600

    feature: repositorios de paciente, nutriologa y empleado actualizados

--- src/main/java/Salud/repository/PacienteRepository.java
+++ src/main/java/Salud/repository/PacienteRepository.java
@@ -4,7 +4,11 @@ import Salud.entity.PacientesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PacienteRepository extends JpaRepository<PacientesEntity, Long> {
+
+    Optional<PacientesEntity> findByCorreo(String correo);
 
 }
